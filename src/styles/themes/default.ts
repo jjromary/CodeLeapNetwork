3171865ba1d0ecf2a520f748739eefd2ed29ef79
Jose Romary <joseromarybrandao@gmail.com>
2023-04-06 20:30:55 -0300

    initial commit: setup project

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+  white: "#fff",
+  black: "#000",
+
+  "blue-500": "#7695EC",
+  "red-500": "#FF5151",
+  "green-500": "#47B960",
+  "gray-300": "#CCCCCC",
+  "gray-500": "#999999",
+  "gray-700": "#777777",
+} as const;
+
